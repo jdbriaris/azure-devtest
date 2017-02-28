020c4daccf1960d41f3abbde33f235e0970ada03
jdbriaris <james.briaris@gmail.com>
2017-02-28 12:15:38 +0000

    Added deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,51 @@
+﻿param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $subscriptionId,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $resourceGroupName,
+
+ [string]
+ $templateFilePath = "azuredeploy.json",
+
+ [string]
+ $parametersFilePath = "azuredeploy.parameters.json"
+ )
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#
+# Assumptions:
+# + You have installed the AzureRM library - InstallModule AzureRM
+# + You have imported the AzureRM library - ImportModule AzureRM
+#******************************************************************************
+$ErrorActionPreference = "Stop";
+
+# sign in
+Write-Host "Logging in...";
+Login-AzureRmAccount;
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+
+# create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# start the deployment
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
